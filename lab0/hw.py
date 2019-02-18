9ea4aff26e9acb7c14d88e738f5b768eafdd9296
jane <zakharova_j@mail.ru>
2019-02-18 20:21:54 +0500

    Add files

---
+++ lab0/hw.py
@@ -0,0 +1,17 @@
+import re
+l=[]
+ls=[]
+s=set()
+with open('input.txt') as f:
+    text = f.read()
+l=re.findall(r'\d+', text)
+for e in l:
+    e=int(e)
+    ls.append(e)
+    s.add(e)
+    ls.sort()
+    if len(s)==1:
+        print("Packet is complete")
+    else:
+        print("These elements aren`t in list:")
+        print(s.symmetric_difference(range(ls[0],ls[len(ls)-1]+1)))
---
+++ lab0/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <math.h>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin("C:\\Users\\123\\Desktop\\6sem\\opersystems\\homework\\input.txt");
+    int k = 0;
+    int x;
+    char q[80];
+    vector<int> v;
+    do
+    {
+        fin >> x;
+        v.push_back(x);
+        k++;
+        //cout<<"ss"<<v.size()<<endl;
+        if(v.size()==1)
+            cout<<"Packet is complete\n";
+        else
+        {
+            sort(v.begin(), v.end());
+            cout<<"These elements aren`t in vector:\n";
+            for(int j=v[0];j<v[v.size()-1];j++)
+            {
+                if(binary_search(v.begin(),v.end(),j)==false)
+                    cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+
+    } while (fin.getline(q, 80));
+//    cout<<v[v.size()-1];
+//    for (int i=0;i<v.size();i++)
+//        cout<<v[i]<<endl;
+    fin.close();
+    return 0;
+}
